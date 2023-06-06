541e7548002579c16fd09db0c96534a3a0d64354
jasonchuacodes <jasonclyde.chua@sun-asterisk.com>
2023-06-06 14:20:51 +0800

    setup: install ts and eslint

---
+++ fe/src/components/base/primary-button.vue
@@ -0,0 +1,12 @@
+<template>
+  <button @click="print"><slot /></button>
+</template>
+
+<script setup lang="ts">
+function print() {
+    console.log('clicked!')
+}
+function unused() {
+    console.log('unused!')
+}
+</script>
