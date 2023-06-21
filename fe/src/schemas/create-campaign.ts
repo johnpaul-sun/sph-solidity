765a26ab168eabb98773d2c0aa02b46bfa307d0a
jasonchuacodes <jasonclyde.chua@sun-asterisk.com>
2023-06-21 14:31:07 +0800

    ui: updated validation rules and error messages for input fields

--- fe/src/schemas/create-campaign.ts
+++ fe/src/schemas/create-campaign.ts
@@ -3,11 +3,16 @@ import * as z from "zod";
 export const CreateCampaignRequestSchema = z.object({
   fullname: z
     .string()
-    .min(5, { message: "Must be 5 or more characters long " }),
+    .min(5, { message: "Minimum character requirement is 5" })
+    .max(30, { message: "Maximum character limit is 30" }),
   campaign: z
     .string()
-    .min(5, { message: "Must be 5 or more characters long " }),
-  story: z.string().min(20, { message: "Must be 20 or more characters long " }),
+    .min(5, { message: "Minimum character requirement is 5" })
+    .max(30, { message: "Maximum character limit is 30" }),
+  story: z
+    .string()
+    .min(20, { message: "Minimum character requirement is 20 " })
+    .max(250, { message: "Maximum character limit is 250" }),
   upload: z.string().optional(),
   goal: z.number({
     required_error: "Goal is required",
