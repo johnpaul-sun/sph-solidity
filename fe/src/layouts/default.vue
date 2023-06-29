c5b687f5dda9d88cdee1e4c5b82246edf961d204
jasonchuacodes <jasonclyde.chua@sun-asterisk.com>
2023-06-29 16:01:57 +0800

    ui: fix horizontal scroll

--- fe/src/layouts/default.vue
+++ fe/src/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative flex flex-col w-screen min-h-screen">
+  <div class="relative flex flex-col min-h-screen">
     <Navbar class="sticky top-0 z-50 backdrop-blur-md" />
     <slot class="z-50" />
     <Footer />
