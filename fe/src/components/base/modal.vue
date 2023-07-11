c6d08bd9358ba5005a63d02bbd021b564fba32ae
Jason Clyde Chua <sirj@Jasons-MacBook-Air.local>
2023-07-11 10:12:13 +0800

    refactor: update modal

--- fe/src/components/base/modal.vue
+++ fe/src/components/base/modal.vue
@@ -1,30 +1,31 @@
 <template>
-  <div v-if="isShowModal">
-    <div class="absolute inset-0 bg-black opacity-25"></div>
-    <div class="fixed inset-0 flex items-center justify-center z-50">
-      <div class="bg-white rounded-md shadow-md w-[400px] overflow-clip">
-        <div
-          class="flex justify-end items-center px-4 h-10 text-dark bg-primary-200"
-        >
-          <BaseButton @click="handleClose">
-            <Icon name="material-symbols:close" class="h-6 w-6" />
-          </BaseButton>
+  <div
+    v-if="isShowModal"
+    class="fixed flex w-screen h-screen justify-center items-center"
+  >
+    <div class="fixed h-screen w-screen top-0 left-0 bg-black opacity-50"></div>
+    <div class="fixed bg-white rounded-md shadow-md w-[400px] overflow-hidden">
+      <div
+        class="flex justify-end items-center px-4 h-10 text-dark bg-primary-200"
+      >
+        <BaseButton @click="handleClose">
+          <Icon name="material-symbols:close" class="h-6 w-6" />
+        </BaseButton>
+      </div>
+      <div
+        class="flex flex-col items-center p-4 space-y-4 text-primary-10 text-sm text-center"
+      >
+        <div>
+          You need to setup and connect your wallet in order to proceed with the
+          transaction.
         </div>
-        <div
-          class="flex flex-col items-center p-4 space-y-4 text-primary-10 text-sm text-center"
+        <NuxtLink
+          to="/help"
+          class="flex w-full justify-center items-center rounded-md px-4 py-2 bg-clip-text text-sm font-bold text-transparent bg-linear-gradient-primary border border-primary-400"
+          @click="handleClose"
         >
-          <div>
-            You need to connect your wallet in order to proceed with the
-            transaction.
-          </div>
-          <NuxtLink
-            to="/help"
-            class="flex w-full justify-center items-center rounded-md px-4 py-2 bg-clip-text text-sm font-bold text-transparent bg-linear-gradient-primary border border-primary-400"
-            @click="handleClose"
-          >
-            Setup MetaMask wallet
-          </NuxtLink>
-        </div>
+          Setup MetaMask wallet
+        </NuxtLink>
       </div>
     </div>
   </div>
--- fe/src/layouts/default.vue
+++ fe/src/layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="relative flex flex-col min-h-screen">
     <Navbar class="sticky top-0 z-50 backdrop-blur-md" />
-    <slot class="z-50" />
     <BaseModal />
+    <slot class="z-50" />
     <Footer />
   </div>
 </template>
