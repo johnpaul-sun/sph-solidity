a76c16a9672c3d6d4bbc63dcd533d2261bc2744c
jasonchuacodes <jasonclyde.chua@sun-asterisk.com>
2023-07-18 11:22:44 +0800

    SOL-81 [Bugifx] add placeholder for tables

--- fe/src/components/user/donations.vue
+++ fe/src/components/user/donations.vue
@@ -1,44 +1,52 @@
 <template>
-  <table class="table-auto mt-6 w-full">
-    <thead class="border-b border-primary-10">
-      <tr class="h-10 items-center">
-        <th class="text-center px-4 text-primary-500 w-1/12">#</th>
-        <th class="text-left px-4 w-3/12">Creator ID</th>
-        <th class="text-left px-4 flex-1 w-6/12">Campaign Title</th>
-        <th class="text-left px-4 w-2/12">Amount</th>
-      </tr>
-    </thead>
-    <tbody v-if="!isLoading">
-      <tr
-        v-for="(
-          { userAddress, campaignTitle, donationAmount }, index
-        ) in donations"
-        :key="index"
-        w
-        class="items-center h-14 min-h-[56px] border-b border-disabled"
-      >
-        <td class="min-w-[56px] px-4 text-center bg-primary-200">
-          {{ index + 1 }}
-        </td>
-        <td class="px-4 py-2">
-          <div class="flex items-center space-x-2">
-            <span>
-              {{ middleTruncate(userAddress, 6, 4) }}
-            </span>
-          </div>
-        </td>
-        <td class="px-4 py-2">{{ campaignTitle }}</td>
-        <td class="px-4 w-40">{{ donationAmount }} ETH</td>
-      </tr>
-    </tbody>
-  </table>
-  <div v-if="lastPage > 1" class="mt-8 h-14 flex items-center justify-center">
-    <BasePaginator
-      :current-page="currentPage"
-      :last-page="lastPage"
-      :items-per-page="itemsPerPage"
-      :on-page-change="setPage"
-    />
+  <div class="mt-6">
+    <table class="table-auto w-full">
+      <thead class="border-b border-primary-10">
+        <tr class="h-10 items-center">
+          <th class="text-center px-4 text-primary-500 w-1/12">#</th>
+          <th class="text-left px-4 w-3/12">Creator ID</th>
+          <th class="text-left px-4 flex-1 w-6/12">Campaign Title</th>
+          <th class="text-left px-4 w-2/12">Amount</th>
+        </tr>
+      </thead>
+      <tbody v-if="!isLoading">
+        <tr
+          v-for="(
+            { userAddress, campaignTitle, donationAmount }, index
+          ) in donations"
+          :key="index"
+          w
+          class="items-center h-14 min-h-[56px] border-b border-disabled"
+        >
+          <td class="min-w-[56px] px-4 text-center bg-primary-200">
+            {{ index + 1 }}
+          </td>
+          <td class="px-4 py-2">
+            <div class="flex items-center space-x-2">
+              <span>
+                {{ middleTruncate(userAddress, 6, 4) }}
+              </span>
+            </div>
+          </td>
+          <td class="px-4 py-2">{{ campaignTitle }}</td>
+          <td class="px-4 w-40">{{ donationAmount }} ETH</td>
+        </tr>
+      </tbody>
+    </table>
+    <div
+      v-if="currentPage !== lastPage"
+      class="mt-8 h-14 flex items-center justify-center"
+    >
+      <BasePaginator
+        :current-page="currentPage"
+        :last-page="lastPage"
+        :items-per-page="itemsPerPage"
+        :on-page-change="setPage"
+      />
+    </div>
+    <div v-if="donations.length < 1" class="flex justify-center mt-6">
+      <p>No donations to show.</p>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
